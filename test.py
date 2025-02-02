0a48837734e318135d78a437adea7b23ac7ea798
Ryan Mastropaolo <ryanmastropaolo2022@gmail.com>
2025-02-02 01:09:27 -0500

    pandas

---
+++ test.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+import pandas as pd
+
+# Read the CSV file into a pandas DataFrame
+df = pd.read_csv('rutgers_university_supermarkets.csv')
+
+# Display the DataFrame
+print(df)
